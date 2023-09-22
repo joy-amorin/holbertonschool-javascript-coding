b0717a5df8ba916738a1d5bf9570f972e1f7247c
Joy-Amorin <amorinkaren2021@gmail.com>
2023-09-22 13:18:02 -0700

    4-starwars_count.js

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const request = require('request');
+let count = 0;
+
+request(process.argv[2], (error, response, data) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  const movies = JSON.parse(data).results;
+  for (const movie of movies) {
+    const characters = movie.characters;
+    for (const characterUrl of characters) {
+      
+      if (characterUrl.includes('18')) {
+        count++;
+      }
+    }
+  }
+
+  console.log(count);
+});
